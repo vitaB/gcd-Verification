bc1d024752ee45ac4579f8d79cdcc0cca2283059
Viktor Befort <v.befort@googlemail.com>
2015-03-22 12:52:40 +0100

    proof mod_diff

--- Coq/ggt.v
+++ Coq/ggt.v
@@ -1,11 +1,25 @@
-Require Import NArith.
-Require Import Bool.
-Require Import Coq.Numbers.Natural.Peano.NPeano.
-Require Import Coq.Arith.Compare_dec.
-Require Import Coq.Numbers.Natural.Abstract.NDiv.
+Require Import NPeano.
 Require Import Omega.
 
 Open Scope nat_scope.
+
+Lemma mod_t : forall n m k,  m > 0 -> (n + k * m) mod m = n mod m.
+Proof.
+  induction k; intro.
+    simpl. rewrite Nat.add_0_r. trivial.
+    rewrite <- IHk. symmetry. rewrite <- (Nat.mod_add _ 1).
+    rewrite <- plus_assoc. rewrite <- Nat.mul_add_distr_r with(p:=m)(n:=k)(m:=1).
+    rewrite Nat.add_1_r. trivial.
+    omega. assumption.
+Qed.
+
+Lemma mod_diff : forall n m : nat, n>=m /\ m <> 0 -> (n - m) mod m = n mod m.
+Proof. 
+  intros n m [H1 H2]. 
+  rewrite <- (Nat.mod_add _ 1). rewrite mult_1_l. rewrite Nat.sub_add. trivial.
+   assumption.
+   assumption.
+Qed.
 
 (* ggT(n,m) = ggT(m, n)
 ggT(0,n) = n
@@ -78,101 +92,12 @@ Proof.
       intros. apply Lt.lt_S. exact H.
 Qed.
 
-Lemma mod_minus : forall n m : nat, n>=m /\ m <> 0 -> (n - m) mod m = n mod m.
-Proof.
-    intros. destruct H as [H1 H2 ]. induction n.
-      rewrite Nat.mod_0_l by omega. rewrite Nat.mod_0_l; omega.
-
-  admit.
-Qed.
-
 Theorem ggT_impl : forall n m : nat, n >= m -> eggT n m = eggT (n - m) m.
 Proof.
-  intros. assert ( H0 : n =  m \/ n >  m) by omega. destruct H0 as [H01 | H02].
-      rewrite H01. rewrite ggT_same by omega; rewrite Nat.sub_diag; trivial.
-      clear H. assert(n - m <> 0) by omega. induction m.
-        rewrite Nat.sub_0_r; trivial.
-        rewrite ggT_kom. rewrite eggT_equation.
-        symmetry. rewrite ggT_kom. rewrite eggT_equation.
-        rewrite mod_minus by omega. trivial.
+  intros. induction m.
+    rewrite Nat.sub_0_r; trivial.
+    rewrite ggT_kom. rewrite eggT_equation.
+    symmetry. rewrite ggT_kom. rewrite eggT_equation. rewrite mod_diff.
+    trivial. omega. 
 Qed.
-
-
-
-
-
-
-
-
-
-
-
-
-
-Section Zahlentheorie.
-Variables a b c r: nat.
-Definition ggT : Set := nat -> nat -> nat.
-Definition eggT : forall (n m : nat), gcd n m = gcd m n.
-Proof. intros. unfold gcd.
-
-Definition ggT (n m : nat) : Set :=  nat -> nat -> nat -> gcd 0 m = m /\ gcd n m = gcd m n.
-Definition eggT_0 : forall m : nat, ggT 0 m.
-Proof. intros. unfold gcd. intros. split. reflexivity. unfold NPeano.gcd. reflexivity. 
-
-
-Definition eggT : forall (n m : nat), gcd n m = gcd m n.
-Proof.  intros. unfold gcd.
-Definition gcd_0 : gcd -> nat := forall n m : nat, n = 0 -> (gcd n m) -> m.
-Inductive gcd (n m : nat) : Set := gcd_def : forall a : nat, n >= m -> a = n - m -> gcd n m.
-(* Spezifikation vom ggT. Vom Typ Set. *)
-(* Definition ggT (a b c : nat) := c. *)
-(* Definition eggT : forall (a b: nat), {(ggT a b) = (ggT b a)} + {(ggT 0 b) -> b }.
-Proof. intros. unfold ggT. auto.  *)
-Definition ggT : Set := nat -> nat -> nat.
-Fixpoint eggT ( a b : nat) :=
-  match a with
-    | 0 => b
-    | S a => eggT(b mod a) (S a)
-end.
-
-Definition divide (a b : nat) : Type := { q | b = a * q }.
-Definition gcd (a b : nat) : nat := prod
-Definition is_gcd d a b :=  prod (divide d a)    (prod (divide d b)      (forall x, divide x a -> divide x b -> divide x d)).Lemma gcd_correct :  forall a b s, Z0 <= a -> Z0 <= b ->{d| prod (        [("gcd", ["a", "b"], gcd_body)]⊢⟨gcd_body |MEM"a"a::MEM"b"b::PROC::s⟩→* ⟨ return #d | MEM "a" a :: MEM "b" b :: PROC :: s ⟩      )      (is_gcd d a b) }.
-
-
-Fixpoint eggT ( a b : nat) : Set :=
- if (a < b ) then
-  match a with
-    | 0 => b
-    | S a => eggT(b mod a) (S a)
-end
-else eggT b a.
-end. 
-
-Definition ggT : forall a b : nat, (ggT a b) = (ggT b a).
-Definition ggT : Set := nat -> nat -> nat.
-Definition ggT_kom := forall a b : nat, (ggT a b) = (ggT b a).
-(* Hypothesis kommutativ : ( forall f : ggT, f a b = f b a). *)
-
-(* Definition eggT : ggT := fun a b=> if (Zeq_bool b 0) then a else 
-  if (Zeq_bool (a mod b) 0 ) then b else 0. *)
-
-(* Theorem kommutativ :  eggT a b = eggT b a. *)
-End Zahlentheorie.
-Eval compute in (5 mod 0).
-
-Fixpoint eggT (a b : Z)  :=
- match a with
-   | 0 => b
-   | Zsucc a' => eggT (b mod a) (Zsucc a)
-  end.
-
-Fixpoint eggT (a b : Z)  :=
-  match b with
-   | 0 => a
-   | _ => let m := (a mod b) in
-       if (Zeq_bool m 0 ) then b else eggT b m 
-  end.
-
-
 Close Scope Z_scope.
