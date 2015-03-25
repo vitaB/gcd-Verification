520b6c3105833d521ce57212781128542b4d2efc
Viktor Befort <v.befort@googlemail.com>
2015-03-25 16:33:28 +0100

    ggt_same proofed

--- Isabelle/ggt.thy
+++ Isabelle/ggt.thy
@@ -1,25 +1,34 @@
 theory ggt
 imports Main
 begin
+
 fun ggt :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
   "ggt 0 m = m"
   | "ggt n m = ggt(m mod n) n"
 
-theorem ggt_o [simp] : "\<forall> n. n = 0 \<longrightarrow> ggt m n = m"
-apply(simp)
-apply(induct_tac m)
-apply(simp)
-apply(simp)
+theorem ggt_o [simp] :  "ggt m 0 = m"
+  apply(induct_tac m)
+  apply(simp)
+  apply(simp)
 done
 
-lemma ggt_mod : "n \<noteq>  0 \<longrightarrow> ggt m n = ggt n (m mod n)"
-apply(rule_tac impI)
+lemma ggt_same : "ggt n n = n"
+  apply (case_tac n)
+  apply (simp)
+  apply (simp)
+done
+
+lemma ggt_mod : " m > 0 \<longrightarrow> ggt n m =  ggt (n mod m) m"
+  apply (case_tac n)
+  apply (simp)
+  apply (subgoal_tac (m = n \<and> m < n \<and> m > n))
 
 theorem ggt_kom : "ggt m n = ggt n m"
-apply(induction m)
-apply(simp)
+  apply (case_tac n)
+  apply (simp)
+  apply (auto)
 
-done
+theorem ggt_impl : "n \<ge> m \<longrightarrow> ggt n m = ggt(n - m) m"
 
 end
 
