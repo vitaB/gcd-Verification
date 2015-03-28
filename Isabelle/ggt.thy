11d07c659f83eaac0dcc35a84588d1024d15e4f8
Viktor Befort <v.befort@googlemail.com>
2015-03-28 23:01:07 +0100

    proof ggt_mod

--- Isabelle/ggt.thy
+++ Isabelle/ggt.thy
@@ -21,15 +21,34 @@ done
 lemma ggt_mod : " m > 0 \<longrightarrow> ggt n m =  ggt (n mod m) m"
   apply (case_tac n)
   apply (simp)
-  apply (subgoal_tac (m = n \<and> m < n \<and> m > n))
+  apply safe
+  apply (case_tac "m = n")
+  apply (simp)
+  apply (case_tac "m < n")
+  apply (simp)
+  apply (auto)
+  apply (case_tac m)
+  apply (auto)
+done
 
 theorem ggt_kom : "ggt m n = ggt n m"
   apply (case_tac n)
   apply (simp)
+  apply safe
+  apply (simp)
+  apply (simp add: ggt_mo)
+  apply (insert ggt_mod)
+  (*apply (rule sym)
+  apply (cut_tac ggt_mod)
   apply (auto)
+  apply (rule ggt_mod)
+  apply (simp add: ggt_mod)
+done*)
+sorry
 
 theorem ggt_impl : "n \<ge> m \<longrightarrow> ggt n m = ggt(n - m) m"
-
+  apply (case_tac m)
+  apply (simp)
 end
 
 
