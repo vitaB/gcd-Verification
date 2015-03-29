201c62bdaf5287a685337c74b437c160a71496c4
Viktor Befort <v.befort@googlemail.com>
2015-03-29 14:08:09 +0200

    proof ggt_kom

--- Isabelle/ggt.thy
+++ Isabelle/ggt.thy
@@ -6,28 +6,29 @@ fun ggt :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
   "ggt 0 m = m"
   | "ggt n m = ggt(m mod n) n"
 
+lemma [simp]: " n = 0 \<Longrightarrow> ggt 0 m = m" by simp
+lemma [simp]: "n \<noteq> 0 \<Longrightarrow> ggt n m = ggt (m mod n) n"
+  apply (case_tac n)
+  apply (auto)
+done
+
 theorem ggt_o [simp] :  "ggt m 0 = m"
   apply(induct_tac m)
-  apply(simp)
-  apply(simp)
+  apply (auto)
 done
 
-lemma ggt_same : "ggt n n = n"
+lemma ggt_same [simp]: "ggt n n = n"
   apply (case_tac n)
-  apply (simp)
-  apply (simp)
+  apply (auto)
 done
 
-lemma ggt_mod : " m > 0 \<longrightarrow> ggt n m =  ggt (n mod m) m"
+lemma ggt_mod : " m > 0 \<longrightarrow> ggt (n mod m) m = ggt n m"
   apply (case_tac n)
   apply (simp)
   apply safe
   apply (case_tac "m = n")
   apply (simp)
   apply (case_tac "m < n")
-  apply (simp)
-  apply (auto)
-  apply (case_tac m)
   apply (auto)
 done
 
@@ -35,20 +36,19 @@ theorem ggt_kom : "ggt m n = ggt n m"
   apply (case_tac n)
   apply (simp)
   apply safe
-  apply (simp)
-  apply (simp add: ggt_mo)
-  apply (insert ggt_mod)
-  (*apply (rule sym)
-  apply (cut_tac ggt_mod)
-  apply (auto)
-  apply (rule ggt_mod)
   apply (simp add: ggt_mod)
-done*)
-sorry
+done
 
 theorem ggt_impl : "n \<ge> m \<longrightarrow> ggt n m = ggt(n - m) m"
   apply (case_tac m)
   apply (simp)
+  using ggt_kom
+  apply (rule ssubst)
+  apply (auto)
+  using le_mod_geq
+  apply (rule subst)
+  apply (auto)
+  (*noch nicht fertig*)
 end
 
 
